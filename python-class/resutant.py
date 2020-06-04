1f32c7eb4c6b9e1ebfe5fbc59886039ec04e6c6a
freedomvictory <dinggl9367@163.com>
2020-06-04 21:01:08 +0800

    add reutant.py(class basic)

---
+++ python-class/resutant.py
@@ -0,0 +1,27 @@
+"""reference book python crash course : passage 142 9-1"""
+class Restruant():
+    """ a restruant"""
+
+    def __init__(self, name, cusine_type):
+        """constructor"""
+        self.name = name
+        self.cusine_type = cusine_type
+
+    def describe_restruant(self):
+        """describe restruant"""
+        print("resruant name:" + self.name)
+        print("cusine_type:" + self.cusine_type)
+
+    def open_restruant(self):
+        """open restruant"""
+        print("open restruant")
+
+"""main function """
+job_restruant = Restruant('job', 'huoGuo')
+mike_restruant = Restruant('mike', 'luCai')
+
+job_restruant.describe_restruant()
+job_restruant.open_restruant()
+
+mike_restruant.describe_restruant()
+mike_restruant.open_restruant()
--- python-list/list_hello.py
+++ python-list/list_hello.py
@@ -1,1 +1,10 @@
-print("hello, world")
+
+n = input("please input 1 or 0:")
+n = int(n)
+if n == 0:
+    print("Hello World")
+elif n > 0:
+    print("He\nll\no \nWo\nrl\nd")
+else:
+    for i in "Hello World":
+        print(i)
